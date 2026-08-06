2101871672c140c808d6029cce1aa35a7a869338
nyarvex <nyarvex@proton.me>
2026-08-06 18:07:40 +0800

    Create uuid.go

---
+++ internal/transport/uuid.go
@@ -0,0 +1,61 @@
+package transport
+
+import (
+    "encoding/hex"
+    "fmt"
+    "strings"
+    "sync"
+
+    "proxy/internal/constants"
+)
+
+var (
+    uuidBytes     []byte
+    uuidOnce      sync.Once
+    uuidInitError error
+)
+
+func InitUUID(uuidStr string) error {
+    uuidOnce.Do(func() {
+        cleaned := strings.ReplaceAll(uuidStr, "-", "")
+        if len(cleaned) != 32 {
+            uuidInitError = fmt.Errorf("invalid UUID length: %d", len(cleaned))
+            return
+        }
+        b, err := hex.DecodeString(cleaned)
+        if err != nil {
+            uuidInitError = err
+            return
+        }
+        if len(b) != constants.UUIDLength {
+            uuidInitError = fmt.Errorf("decoded UUID length != 16: %d", len(b))
+            return
+        }
+        uuidBytes = b
+    })
+    return uuidInitError
+}
+
+func GetUUIDBytes() []byte {
+    if uuidBytes == nil {
+        return nil
+    }
+    copyBytes := make([]byte, constants.UUIDLength)
+    copy(copyBytes, uuidBytes)
+    return copyBytes
+}
+
+func MatchUUID(data []byte) bool {
+    if len(data) < constants.UUIDLength+1 {
+        return false
+    }
+    if uuidBytes == nil || len(uuidBytes) != constants.UUIDLength {
+        return false
+    }
+    for i := 0; i < constants.UUIDLength; i++ {
+        if data[1+i] != uuidBytes[i] {
+            return false
+        }
+    }
+    return true
+}
